e3c2d98993bf9e3a2084cd39f37bb1b290818cc2
pasqu001 <kaseypasqualini@yahoo.com>
2019-11-02 22:13:30 -0400

    finish project

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -63,13 +63,15 @@ namespace TechJobsConsole
                     // Fetch results
                     if (columnChoice.Equals("all"))
                     {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                        searchResults = JobData.FindByValue(searchTerm);
+                        PrintJobs(searchResults);
                     }
                     else
                     {
                         searchResults = JobData.FindByColumnAndValue(columnChoice, searchTerm);
                         PrintJobs(searchResults);
                     }
+                    
                 }
             }
         }
@@ -118,7 +120,17 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("printJobs is not implemented yet");
+            foreach (Dictionary<string, string> job in someJobs)
+            {
+                Console.WriteLine("**");
+                foreach (KeyValuePair<string, string> section in job)
+                {
+                    Console.WriteLine("{0}: {1}",
+                    section.Key, section.Value);
+                }
+                Console.WriteLine("**");
+                Console.WriteLine(Environment.NewLine);
+            }
         }
     }
 }
